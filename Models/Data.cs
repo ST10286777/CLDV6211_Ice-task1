74837a20ac9700e27500d8855df0367fbc3713e3
Keira Wilmot <128458043+ST10286777@users.noreply.github.com>
2024-03-20 16:53:37 +0200

    Add files via upload

---
+++ Models/Data.cs
@@ -0,0 +1,58 @@
+﻿using CLDV_ICE_TASK.Controllers;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+using CLDV_ICE_TASK.Models;
+using CLDV_ICE_TASK.Data;
+
+namespace IceTask1.Models;
+
+public static class Data
+{
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using (var context = new CLDV_ICE_TASKContext(
+            serviceProvider.GetRequiredService<
+                DbContextOptions<CLDV_ICE_TASKContext>>()))
+        {
+            // Look for any task.
+            if (context.TaskManager.Any())
+            {
+                return;   // DB has been seeded
+            }
+            context.TaskManager.AddRange(
+                new TaskManager
+                {
+                    TaskName = "CLDV- Ice task 1",
+                    Taskdetails = "Create web app",
+                    Deadline = DateTime.Parse("2024-5-12"),
+                    Status = "Completed"
+
+                },
+                new TaskManager
+                {
+                    TaskName = "CLDV- Ice task 1",
+                    Taskdetails = "Create web app",
+                    Deadline = DateTime.Parse("2024-5-12"),
+                    Status = "Completed"
+                },
+                new TaskManager
+                {
+                    TaskName = "CLDV- Ice task 1",
+                    Taskdetails = "Create web app",
+                    Deadline = DateTime.Parse("2024-5-12"),
+                    Status = "Completed"
+                },
+                new TaskManager
+                {
+                    TaskName = "CLDV- Ice task 1",
+                    Taskdetails = "Create web app",
+                    Deadline = DateTime.Parse("2024-5-12"),
+                    Status = "Completed"
+                }
+            );
+            context.SaveChanges();
+        }
+    }
+}
---
+++ Models/TaskManager.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CLDV_ICE_TASK.Models
+{
+    public class TaskManager
+    {
+        public int Id { get; set; }
+        public string TaskName { get; set; }
+        public string? Taskdetails { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime Deadline { get; set; }
+        public string Status { get; set; }
+    }
+}
